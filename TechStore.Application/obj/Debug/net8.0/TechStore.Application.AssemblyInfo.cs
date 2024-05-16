f451090b93695cb1975c9b683b89a2ea3ba32537
Kuro Hiyuki <030236200121@st.buh.edu.vn>
2024-05-16 11:56:09 +0700

    Config validation for registation

--- TechStore.Application/obj/Debug/net8.0/TechStore.Application.AssemblyInfo.cs
+++ TechStore.Application/obj/Debug/net8.0/TechStore.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechStore.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+444a05dfc17d4965ae6b51343cb210cde1db7765")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4053a5a553c302f1c37d6a827953fb3a9afae5b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechStore.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechStore.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
